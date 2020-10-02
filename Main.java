3a682a645aec55c11771bb9ed381c212c238f512
aydig8139 <aydig8139@wrdsb.ca>
2020-10-02 15:57:14 +0000

    added comments

--- Main.java
+++ Main.java
@@ -18,23 +18,24 @@ public class Main {
     System.out.println("Please enter a positive integer to see the FizzBuzz numbers");
     int usersNum = input.nextInt();
 
-    //declare variables to count from
+    //declare a variable to count from
     int count = 0;
-
-   
 
     //create while loop
     while (usersNum > count){
       
       count = count + 1;
+     
       //create constant for divisibility by 3
       final int DIV_BY_3 = count % 3;
 
       //create constant for divisibility by 5
       final int DIV_BY_5 = count % 5;
 
-       //create a constant for a number that means the division has no remainders
+      //create a constant for a number that means the division has no remainders
       final int NO_REMAINDER = 0;
+      
+      //create if statement to check the users number is divisible by 3, 5 or both
       if  (DIV_BY_3 == NO_REMAINDER && DIV_BY_5 == NO_REMAINDER ) {
        System.out.println("FizzBuzz");
      } else if (DIV_BY_5 == NO_REMAINDER){
